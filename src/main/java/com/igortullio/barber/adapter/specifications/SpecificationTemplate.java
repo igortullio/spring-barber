5dba04f3ee19d493260cfd079f4bdb196f229c04
Igor Tullio <igortullio@hotmail.com>
2021-11-18 11:19:08 -0300

    Add hibernate-jpamodelgen for help in specifications

--- src/main/java/com/igortullio/barber/adapter/specifications/SpecificationTemplate.java
+++ src/main/java/com/igortullio/barber/adapter/specifications/SpecificationTemplate.java
@@ -1,12 +1,19 @@
 package com.igortullio.barber.adapter.specifications;
 
+import com.igortullio.barber.adapter.database.entity.AbstractEntity_;
 import com.igortullio.barber.adapter.database.entity.AddressEntity;
+import com.igortullio.barber.adapter.database.entity.AddressEntity_;
 import com.igortullio.barber.adapter.database.entity.BarbershopEntity;
+import com.igortullio.barber.adapter.database.entity.BarbershopEntity_;
 import com.igortullio.barber.adapter.database.entity.CityEntity;
+import com.igortullio.barber.adapter.database.entity.CityEntity_;
 import com.igortullio.barber.adapter.database.entity.OperationEntity;
+import com.igortullio.barber.adapter.database.entity.OperationEntity_;
 import com.igortullio.barber.adapter.database.entity.PermissionEntity;
 import com.igortullio.barber.adapter.database.entity.PermissionGroupEntity;
+import com.igortullio.barber.adapter.database.entity.PermissionGroupEntity_;
 import com.igortullio.barber.adapter.database.entity.ScheduleEntity;
+import com.igortullio.barber.adapter.database.entity.ScheduleEntity_;
 import com.igortullio.barber.adapter.database.entity.StateEntity;
 import com.igortullio.barber.adapter.database.entity.UserEntity;
 import com.igortullio.barber.adapter.util.SecurityUtil;
@@ -25,28 +32,28 @@ public class SpecificationTemplate {
     public static Specification<CityEntity> cityStateId(final Long stateId) {
         return (root, query, cb) -> {
             query.distinct(true);
-            Join<CityEntity, StateEntity> join = root.join("state");
+            Join<CityEntity, StateEntity> join = root.join(CityEntity_.STATE);
 
-            return cb.equal(join.get("id"), stateId);
+            return cb.equal(join.get(AbstractEntity_.ID), stateId);
         };
     }
 
     public static Specification<BarbershopEntity> barbershopCityId(final Long cityId) {
         return (root, query, cb) -> {
             query.distinct(true);
-            Join<BarbershopEntity, AddressEntity> joinAddress = root.join("address");
-            Join<AddressEntity, CityEntity> joinCity = joinAddress.join("city");
+            Join<BarbershopEntity, AddressEntity> joinAddress = root.join(BarbershopEntity_.ADDRESS);
+            Join<AddressEntity, CityEntity> joinCity = joinAddress.join(AddressEntity_.CITY);
 
-            return cb.equal(joinCity.get("id"), cityId);
+            return cb.equal(joinCity.get(AbstractEntity_.ID), cityId);
         };
     }
 
     public static Specification<OperationEntity> operationBarbershopId(Long barbershopId) {
         return (root, query, cb) -> {
             query.distinct(true);
-            Join<OperationEntity, BarbershopEntity> joinBarbershop = root.join("barbershop");
+            Join<OperationEntity, BarbershopEntity> joinBarbershop = root.join(OperationEntity_.BARBERSHOP);
 
-            return cb.equal(joinBarbershop.get("id"), barbershopId);
+            return cb.equal(joinBarbershop.get(AbstractEntity_.ID), barbershopId);
         };
     }
 
@@ -58,11 +65,11 @@ public class SpecificationTemplate {
             List<Predicate> predicateList = new ArrayList<>();
 
             if (Objects.isNull(operationId)) {
-                Join<ScheduleEntity, UserEntity> joinUser = root.join("user");
-                predicateList.add(cb.equal(joinUser.get("id"), userLogged.getId()));
+                Join<ScheduleEntity, UserEntity> joinUser = root.join(ScheduleEntity_.USER);
+                predicateList.add(cb.equal(joinUser.get(AbstractEntity_.ID), userLogged.getId()));
             } else {
-                Join<ScheduleEntity, OperationEntity> joinOperation = root.join("operation");
-                predicateList.add(cb.equal(joinOperation.get("id"), operationId));
+                Join<ScheduleEntity, OperationEntity> joinOperation = root.join(ScheduleEntity_.OPERATION);
+                predicateList.add(cb.equal(joinOperation.get(AbstractEntity_.ID), operationId));
             }
 
             Predicate[] predicates = predicateList.toArray(new Predicate[0]);
@@ -77,9 +84,9 @@ public class SpecificationTemplate {
 
         return (root, query, cb) -> {
             query.distinct(true);
-            Join<PermissionGroupEntity, PermissionEntity> joinPermission = root.join("permissionSet");
+            Join<PermissionGroupEntity, PermissionEntity> joinPermission = root.join(PermissionGroupEntity_.PERMISSION_SET);
 
-            return cb.equal(joinPermission.get("id"), permissionId);
+            return cb.equal(joinPermission.get(AbstractEntity_.ID), permissionId);
         };
     }
 
